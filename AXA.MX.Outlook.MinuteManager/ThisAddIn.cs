061cd538144e122d656c487481a413b603aa57c8
MXE01505538A <MXE01505538A@MXALPRCA1FW5QC2>
2016-11-24 13:39:29 -0600

    Add project files.

---
+++ AXA.MX.Outlook.MinuteManager/ThisAddIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace AXA.MX.Outlook.MinuteManager
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Nota: Outlook ya no genera este evento. Si tiene código que 
+            //    deba ejecutarse cuando Outlook se cierre, vea http://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region Código generado por VSTO
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
